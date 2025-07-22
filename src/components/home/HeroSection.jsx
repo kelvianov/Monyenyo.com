b63f51ec41d2bbc54fa7e5f9acd113a537ebf6aa
mpii <ferry4biz@gmail.com>
2025-07-23 02:39:07 +0700

    Update title in HeroSection component for better branding

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
     {
       image: desktop1,
       subtitle: "Lembut. Gurih. Nagih.",
-      title: "NIKMAT GURIHNYA!<br />BOLU ABON MONYENYO"
+      title: "NIKMAT GURIHNYA!<br />BOLU sssssssssssssssssssss"
     },
     {
       image: desktop2,
