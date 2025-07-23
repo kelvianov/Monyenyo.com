e4a6da1170dcecfc4b26e98ea6dee3cea3cb2d4f
mpii <ferry4biz@gmail.com>
2025-07-23 10:35:33 +0700

    Fix base path in vite.config.js for correct asset loading

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',   // Tambahkan ini supaya path asset relatif
+  base: '/',   // Tambahkan ini supaya path asset relatif
   plugins: [react()],
   server: {
     port: 3000,
