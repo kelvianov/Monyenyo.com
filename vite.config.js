badd40a82e4e56b5db0d04f8ade8950a66c6d242
mpii <ferry4biz@gmail.com>
2025-07-23 10:50:35 +0700

    Refactor vite.config.js by removing unused server and publicDir configurations

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: './',   // Tambahkan ini supaya path asset relatif
+  base: './',
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
-  },
-  publicDir: 'public'
+  }
 })
